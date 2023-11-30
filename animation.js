4c755cb187d5520fb2eec5fd54abe85d093d4e63
janedaniels <126726466+janedaniels@users.noreply.github.com>
2023-11-30 23:10:58 +0200

    30.11_2

---
+++ animation.js
@@ -0,0 +1,8 @@
+
+
+// const burger = document.querySelector('.burger')
+// const menu = document.querySelector('.menu')
+
+// burger.addEventListener('click', function () {
+//     menu.classList.toggle('active');
+// })
