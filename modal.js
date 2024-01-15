3246b866bf7b47c0a3ca11dc812557d23e182b92
janedaniels <126726466+janedaniels@users.noreply.github.com>
2024-01-16 00:50:23 +0200

    plus search html css

--- modal.js
+++ modal.js
@@ -1,6 +1,6 @@
-
+// визначаємо контейнер для каталогу книг
 const bookList = document.querySelector(".book-list-js");
-
+// формуємо запит на отримання даних з локального json файлу 
 const requestURL = "./books.json";
 const request = new XMLHttpRequest();
 request.open('GET', requestURL, false);
@@ -9,28 +9,9 @@ request.send();
 const dataBooks = JSON.parse(request.responseText);
   if (request.status === 200) { dataBooks }
     else { console.error(request.statusText) };
-
-// const id = dataBooks.books[0].id;
-// const author = dataBooks.books[0].author;
-// const price = dataBooks.books[0].price;
-// const image = dataBooks.books[0].image;
-// const title = dataBooks.books[0].title;
-// const level = dataBooks.books[0].level;
-
+// виймаємо масив об'єктів з отриманого об'екту 
 const bookArray = dataBooks.books;
-
-
-// function createMarkap(element) {
-//   return element.innerHTML = `<li>
-// <img src="${image}" alt="books image" width="200"/>
-// <h2 id="${id}">${title}</h2>
-// <h3>${author}</h3>
-// <h4><p>Price ${price}</p></h4>
-// <button>View</button>
-// </li>`
-// };
-
-
+// функція для отримання розмітки каталогу, передаємо масив і елемент що треба наповнити
 function createElementList(arr, element) {
 const markup = arr.map(({ image, id, title, author, price }) => `<li>
   <img src="${image}" alt="books image" width="250"/>
@@ -39,6 +20,7 @@ const markup = arr.map(({ image, id, title, author, price }) => `<li>
 <h4><p>${price} $</p></h4>
 <button>View</button>
 </li>`).join('');
+// вставляємо в документ
   element.insertAdjacentHTML('beforeend', markup);
 }
 createElementList(bookArray, bookList)
@@ -61,6 +43,73 @@ bookItemsCollection.forEach(bookItem => {
 });
 
   console.log(bookItemsCollection);
+
+
+
+// =============================================ВАЛІДАЦІЯ ФОРМИ==================================
+// document.addEventListener('DOMContentLoaded', function () {
+//   const formSubscribe = document.querySelector('.modal-form');
+//   console.log(formSubscribe);
+//   formSubscribe.addEventListener('submit', formSend);
+
+//   async function formSend(e) {
+//     e.preventDefault();
+
+//     let error = formValidate(formSubscribe);
+//     let formData = new formData(formSubscribe);
+    
+//     if (error === 0) {
+//       let response = await fetch('sendmail.php', {
+//         method: 'POST',
+//         value: formData
+//       });
+//       if (response.ok) {
+//         let result = await response.json;
+//         alert(result.message);
+//         formSubscribe.reset();
+//        }
+//       else {alert(error)};
+//     }
+//     else { alert('Заповніть потрібне поле') }
+
+//     function formValidate(formSubscribe) {
+//       let error = 0;
+//       let formReq = document.querySelectorAll('._req');
+//       for (let i = 0; i < formReq.length, i += 1;) {
+//         const input = formReq[i];
+//         formRemoveError(input);
+
+    //     if (input.classList.contains('_email')) {
+    //       if (emailTest(input)) {
+    //         formAddError(input);
+    //         error++;
+    //       }
+    //       else {
+    //         if (input.value === '') {
+    //           formAddError(input);
+    //           error++;
+    //         }
+    //       }
+    //     }
+
+    //   }
+    // }
+//     return error
+//   }
+
+//   function formAddError(input) {
+//     input.parentElement.classList.add('_error');
+//     input.classList.add('_error');
+//   }
+//   function formRemoveError(input) {
+//     formSubscribe.classList.remove('_error');
+//     input.classList.remove('_error');
+//   }
+//   // перевірка імейлу
+//   function emailTest(input) {
+//     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//   }
+// })
 
 
 
